015b15455a498b81285458ebb0ef338b43994689
Immanuel Sun <immanuelsun@newstreamsdesign.com>
2014-11-08 22:19:44 -0800

    added CoffeeScript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
-var gulp = require('gulp');
-    gutil = require('gulp-util');
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee');
 
-gulp.task('log', function() {
-    gutil.log('Workflows are awesome');
+var coffeeSource = ['components/coffee/*.coffee'];
+
+gulp.task('coffee', function() {
+    gulp.src(coffeeSource)
+        .pipe(coffee({ bare: true })
+            .on('error', gutil.log))
+        .pipe(gulp.dest('components/scripts'))
 });
