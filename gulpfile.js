fd2591a6248915ac2dcd121752ef10184157b848
Immanuel Sun <immanuelsun@newstreamsdesign.com>
2014-11-09 16:08:05 -0800

    added static file monitoring

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+// define functions based on installed plugins
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
@@ -6,6 +7,8 @@ var gulp = require('gulp'),
     connect = require('gulp-connect'),
     concat = require('gulp-concat');
 
+// define file sources
+var htmlSources = ['builds/development/*.html'];
 var coffeeSources = ['components/coffee/*.coffee'];
 var jsSources = [
     'components/scripts/pixgrid.js',
@@ -14,6 +17,7 @@ var jsSources = [
     'components/scripts/template.js'
 ];
 var sassSources = ['components/sass/style.scss'];
+var jsonSources = ['builds/development/js/*.json'];
 
 gulp.task('coffee', function() {
     gulp.src(coffeeSources)
@@ -42,11 +46,16 @@ gulp.task('compass', function() {
         .pipe(connect.reload())
 });
 
-gulp.task('watch', function() {
-    gulp.watch(coffeeSources, ['coffee']);
-    gulp.watch(jsSources, ['js']);
-    gulp.watch('components/sass/*.scss', ['compass']);
+gulp.task('html', function() {
+    gulp.src(htmlSources)
+        .pipe(connect.reload())
 });
+
+gulp.task('json', function() {
+    gulp.src(jsonSources)
+        .pipe(connect.reload())
+});
+
 
 gulp.task('connect', function() {
     connect.server({
@@ -55,4 +64,12 @@ gulp.task('connect', function() {
     });
 });
 
-gulp.task('default', ['coffee', 'js', 'compass', 'connect', 'watch']);
+gulp.task('watch', function() {
+    gulp.watch(htmlSources, ['html']);
+    gulp.watch(jsonSources, ['json']);
+    gulp.watch(coffeeSources, ['coffee']);
+    gulp.watch(jsSources, ['js']);
+    gulp.watch('components/sass/*.scss', ['compass']);
+});
+
+gulp.task('default', ['html', 'json', 'coffee', 'js', 'compass', 'connect', 'watch']);
